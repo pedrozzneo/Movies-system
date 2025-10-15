acc784240d707f844317205cfa8fb642447b0f6a
pedroNeo <pedrozzneo@gmail.com>
2025-10-15 03:11:31 -0300

    feat: add file logic handling

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 def main():
-    print("test")
+    # Abre o arquivo, salva seu conteúdo divido por linhas em uma variável local e depois fecha arquivo
+    arquivo = open("users.txt")
+    conteudo = arquivo.readlines()
+    arquivo.close()
+
+    # Extrai a chave e seus atributos organizados a cada linha separados por /
+    user_dict = {}
+    for linha in conteudo:
+        elementos = linha.split("/")
+        user_dict[elementos[0]] = [elementos[1], elementos[2]]
+
+    print(user_dict)
 main()
