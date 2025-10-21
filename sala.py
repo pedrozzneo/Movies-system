293932898af9b9663a8e88bb875f095e6fe8aec3
pedroNeo <pedrozzneo@gmail.com>
2025-10-21 02:05:29 -0300

    refactor: now create the dict through function

--- sala.py
+++ sala.py
@@ -96,18 +96,20 @@ def excluir(sala_dict):
     file.writelines(content)
     file.close()
     
-    
-def main():
+def build_dict(sala_dict):
     # Abre o arquivo, salva seu conteúdo divido por linhas em uma variável local e depois fecha arquivo
     arquivo = open("arquivos/sala.txt")
     conteudo = arquivo.readlines()
     arquivo.close()
 
     # Extrai a chave e seus atributos organizados a cada linha separados por /
-    sala_dict = {}
     for linha in conteudo:
         elementos = linha.split("/")
         sala_dict[elementos[0]] = [elementos[1], elementos[2], elementos[3], elementos[4]]
+
+def main():
+    sala_dict = {}
+    build_dict(sala_dict)
 
     escolha = 0
     while escolha != 5:
