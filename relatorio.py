41f6d4487537221036e8daa55c60dfb99ac8a54b
pedroNeo <pedrozzneo@gmail.com>
2025-10-24 12:43:02 -0300

    feat: filter session by dates

--- relatorio.py
+++ relatorio.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 def menu():
     # Força uma entrada válida para escolha
     escolha = 0
@@ -37,16 +39,37 @@ def listarFilmeAPartirAno(filme_dict):
     ano = int(input("Ano: "))
     for item in filme_dict.items():
         if int(item[1][1]) >= ano:
-            print(f"Código: {item[0]}// Nome: {item[1][0]}// Ano de lançamento: {item[1][1]}// Diretor {item[1][2]}// Atores: {item[1][3]}") 
+            print(f"Código: {item[0]}// Nome: {item[1][0]}// Ano de lançamento: {item[1][1]}// Diretor {item[1][2]}// Atores: {item[1][3]}")
+
+def listSessionFromDateToDate(session_dict):
+    # Collect and format fromDate
+    fromDate = input("Data início (DD-MM-AAAA): ").split("-")
+    fromDate = date(int(fromDate[2]), int(fromDate[1]), int(fromDate[0]))
+    print(fromDate)
+
+    # Collect and format toDate 
+    toDate = input("Data final: ").split("-")
+    toDate = date(int(toDate[2]), int(toDate[1]), int(toDate[0]))
+    print(toDate)
+
+    # Something
+    for item in session_dict.items():
+        # Collect and format sessionDate
+        sessionDate = item[1][1].split("-")
+        sessionDate = date(int(sessionDate[2]), int(sessionDate[1]), int(sessionDate[0]))
+        print(sessionDate)
+
+        if sessionDate >= fromDate and sessionDate <= toDate:
+            print(f"Código do Filme: {item[0]} // Código da Sala: {item[1][0]} // Data: {item[1][1]} // Horário: {item[1][2]} // Preço do Ingresso: {item[1][3]}")
 
 def main():
     # Declara e monta o dicionário da sala, filme e sessao
     sala_dict = {}
     filme_dict = {}
-    sessao_dict = {}
+    session_dict = {}
     build_dict_through_file(sala_dict, "sala")
     build_dict_through_file(filme_dict, "filme")
-    build_dict_through_file(sessao_dict, "sessao")
+    build_dict_through_file(session_dict, "sessao")
 
     # Continua oferecendo opções até o usuário decidir sair (6)
     escolha = 0
@@ -58,8 +81,8 @@ def main():
            listarSalaPorExibicaoCapacidade(sala_dict)
         elif escolha == 2:
             listarFilmeAPartirAno(filme_dict)
-        # elif escolha == 3:
-        #     incluir(sala_dict)
+        elif escolha == 3:
+            listSessionFromDateToDate(session_dict)
         elif escolha == 4:
             return
             
